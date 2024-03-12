316953b068cbb339ff2cb22659fa5abf15740cb1
Jedeon SAP LONTCHI <sdevsap@gmail.com>
2024-03-11 23:16:22 -0400

    Ajout de la nav bar et les action lors du clique sur une option

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 // import logo from './logo.svg';
-import './App.css';
-import MapComponent from './components/MapComponent';
+
+import React, { useState } from 'react';
+import MapComponent from "./components/MapComponent";
+import NavBar from "./components/NavBar";
 
 function App() {
+
+  const handleTypeSelect = (type) => {
+    console.log(`Type de Propriété sélectionné : ${type}`);
+    // Implémentons la logique de filtrage ici
+  };
+
+  const handleLocationSelect = (location) => {
+    console.log(`Emplacement sélectionné : ${location}`);
+    // Implémentons la logique de filtrage ici
+  };
+
   return (
     <div className="App">
-      <MapComponent googleMapsApiKey = ""/>
+      <NavBar onTypeSelect={handleTypeSelect} onLocationSelect={handleLocationSelect} />
+      <MapComponent googleMapsApiKey="" />
     </div>
   );
 }
