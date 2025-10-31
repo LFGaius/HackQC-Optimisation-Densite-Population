70fa8dfa3d7c3784fca5421141fd7d288cb28274
LFGaius <53369728+LFGaius@users.noreply.github.com>
2025-10-31 13:03:37 -0400

    Initialize SalesforceInteractions in App component
    Initialize SalesforceInteractions with consent settings and set logging level.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import NavBar from "./components/nav/NavBar";
 import Body from './components/body/Body';
 
 function App() {
-
+SalesforceInteractions.init({
+            consents: [{
+                provider: 'ExampleProvider',
+                purpose: 'Tracking',
+                status: SalesforceInteractions.ConsentStatus.OptIn
+            }]
+        })
+        .then(() => {
+            console.log('Consent set successfully');
+        })
+        
+        // Set the logging level for debugging purposes
+        SalesforceInteractions.setLoggingLevel(4); 
   return (
     <div className="App">
       <NavBar />
