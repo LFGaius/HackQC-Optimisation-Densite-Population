77ee76b4545eaae639ba2c12bf48d1d32580ce24
Sdev237 <sdevsap@gmail.com>
2026-01-30 21:24:03 -0500

    update MapComponent.jsx  File

--- src/components/body/MapComponent.jsx
+++ src/components/body/MapComponent.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
 import { database } from '../../firebase-config';
-import { ref, onValue } from 'firebase/database';
+import { ref, set, onValue } from 'firebase/database';
 import busStopIcon from '../../assets/images/bus-stop.png';
 import publicPlaceIcon from '../../assets/images/public-place.png';
 import buildingIcon from '../../assets/images/build.png';
@@ -47,45 +47,32 @@ const MapComponent = ({filtersMap}) => {
   const [dataProperties, setDataProperties] = useState({ montreal : [], sherbrooke : [], shawinigan : []});
   
 
-  const onClickMarker = (marker) => {
+  const onClickMarker = (marker) => {debugger
     const currentMarker = allMarkers.find(m=>marker.title === m.title);
     let infowindow = new window.google.maps.InfoWindow({
-      content: `
-      ${currentMarker.adresse_principale ? '<b style = "font-weight:bold;">Adresse Principale : </b>' + currentMarker.adresse_principale + '</br>' : ''}  
-      ${currentMarker.telephone ? '<b style = "font-weight:bold;">Téléphone : </b>' + currentMarker.telephone + '</br>' : ''}  
-      ${currentMarker.courriel ? '<b style = "font-weight:bold;">Courriel : </b>' + currentMarker.courriel + '</br>' : ''}  
-      ${currentMarker.description ? '<b style = "font-weight:bold;">Description : </b>' + currentMarker.description + '</br>' : ''}  
-      ${currentMarker.types ? '<b style = "font-weight:bold;">Types : </b>' + currentMarker.types + '</br>' : ''}  
-      ${currentMarker.installations ? '<b style = "font-weight:bold;">Installations : </b>' + currentMarker.installations + '</br>' : ''}  
-      ${currentMarker.activites ? '<b style = "font-weight:bold;">Activites : </b>' + currentMarker.activites + '</br>' : ''}  
-      ${currentMarker.statut_ouverture ? '<b style = "font-weight:bold;">Statut Ouverture : </b>' + currentMarker.statut_ouverture + '</br>' : ''}  
-      ${currentMarker.horaire_par_jour ? '<b style = "font-weight:bold;">Horaire Par Jour : </b>' + currentMarker.horaire_par_jour + '</br>' : ''}  
-      ${currentMarker.paiement ? '<b style = "font-weight:bold;">Paiement : </b>' + currentMarker.paiement + '</br>' : ''}  
-
-      ${currentMarker.id_permis? '<b style = "font-weight:bold;">Numéro du permis: </b>' + currentMarker.id_permis : ''} 
-      ${currentMarker.date_emission? '</br><b style = "font-weight:bold;">Date d\'émission: </b>' + currentMarker.date_emission : ''} 
-      ${currentMarker.emplacement? '</br><b style = "font-weight:bold;">Adresse concernée: </b>' + currentMarker.emplacement : ''} 
-      ${currentMarker.arrondissement? '</br><b style = "font-weight:bold;">arrondissement: </b>' + currentMarker.arrondissement : ''} 
-      ${currentMarker.description_type_demande? '</br><b style = "font-weight:bold;">Description de la demande : </b>' + currentMarker.description_type_demande : ''} 
-      ${currentMarker.description_type_batiment? '</br><b style = "font-weight:bold;">Description du type de bâtiment : </b>' + currentMarker.description_type_batiment : ''} 
-      ${currentMarker.nature_travaux? '</br><b style = "font-weight:bold;">Nature des travaux: </b>' + currentMarker.nature_travaux : ''} 
-      ${currentMarker.nb_logements? '</br><b style = "font-weight:bold;">Nombre de logements: </b>' + currentMarker.nb_logements : ''} 
-
-      ${currentMarker.direction ? '<b style = "font-weight:bold;">Direction: </b>' + currentMarker.direction : ''}
-      ${currentMarker.mob_reduit? '<b style = "font-weight:bold;">Accès à mobilité réduite: </b>' + currentMarker.mob_reduit : ''} 
-      ${currentMarker.hvoiture_n? '<b style = "font-weight:bold;">Navette 1: </b>' + currentMarker.hvoiture_n : ''} 
-      ${currentMarker.hvoiture_nw ? '<b style = "font-weight:bold;">Navette 2: </b>' + currentMarker.hvoiture_nw : ''}
-
-      ${currentMarker.notes ? '<b style = "font-weight:bold;">notes: </b>' + currentMarker.notes : ''}
-      ${currentMarker.buildingType ? '<b style = "font-weight:bold;">Type de batiment: </b>' + currentMarker.buildingType : ''}
-      ${currentMarker.unitsAvailable ? '</br><b style = "font-weight:bold;">Unites disponibles: </b>' + currentMarker.unitsAvailable : ''}
-      ${currentMarker.status ? '</br><b style = "font-weight:bold;">Status: </b>' + currentMarker.status : ''}
-      ${currentMarker.deliveryDate ? '</br><b style = "font-weight:bold;">Date de livraison: </b>' + currentMarker.deliveryDate : ''}
-      ${currentMarker.address ? '</br><b style = "font-weight:bold;">Addresse: </b>' + currentMarker.address : ''}
-      ${currentMarker.contact ? '</br><b style = "font-weight:bold;">Contact: </b>' + currentMarker.contact : ''}
+      content: `${currentMarker.titre_lieu ? '<b>titre_lieu : </b>' + currentMarker.titre_lieu  : ''}  ${currentMarker.id_permis? '<b>id_permis: </b>' + currentMarker.id_permis : ''} 
+      ${currentMarker.date_debut? '</br><b>date_debut: </b>' + currentMarker.date_debut : ''} 
+      ${currentMarker.date_emission? '</br><b>date_emission: </b>' + currentMarker.date_emission : ''} 
+      ${currentMarker.emplacement? '</br><b>emplacement: </b>' + currentMarker.emplacement : ''} 
+      ${currentMarker.arrondissement? '</br><b>arrondissement: </b>' + currentMarker.arrondissement : ''} 
+      ${currentMarker.code_type_base_demande? '</br><b>code_type_base_demande: </b>' + currentMarker.code_type_base_demande : ''} 
+      ${currentMarker.description_type_demande? '</br><b>description_type_demande: </b>' + currentMarker.description_type_demande : ''} 
+      ${currentMarker.description_type_batiment? '</br><b>description_type_batiment: </b>' + currentMarker.description_type_batiment : ''} 
+      ${currentMarker.nature_travaux? '</br><b>nature_travaux: </b>' + currentMarker.nature_travaux : ''} 
+      ${currentMarker.nb_logements? '</br><b>nb_logements: </b>' + currentMarker.nb_logements : ''} 
+      ${currentMarker.longitude? '<b>longitude: </b>' + currentMarker.longitude : ''} 
+      ${currentMarker.latitude? '<b>latitude: </b>' + currentMarker.latitude : ''} 
+      ${currentMarker.direction ? '<b>direction: </b>' + currentMarker.direction : ''}
+      ${currentMarker.notes ? '<b>notes: </b>' + currentMarker.notes : ''}
+      ${currentMarker.buildingType ? '<b>Type de batiment: </b>' + currentMarker.buildingType : ''}
+      ${currentMarker.unitsAvailable ? '</br><b>Unites disponibles: </b>' + currentMarker.unitsAvailable : ''}
+      ${currentMarker.status ? '</br><b>Status: </b>' + currentMarker.status : ''}
+      ${currentMarker.deliveryDate ? '</br><b>Date de livraison: </b>' + currentMarker.deliveryDate : ''}
+      ${currentMarker.address ? '</br><b>Addresse: </b>' + currentMarker.address : ''}
+      ${currentMarker.contact ? '</br><b>Contact: </b>' + currentMarker.contact : ''}
       `,
       position: currentMarker.position,
-      maxWidth: 300
+      maxWidth: 200
     });
 
     infowindow.open(
@@ -148,15 +135,9 @@ const MapComponent = ({filtersMap}) => {
         fillColor = '#F0741F';
         fillOpacity = 0.1;
       } else if(feature.getProperty('ADIDU')) {//Indice equite vie
-        strokeColor = 'red';
-        fillColor = 'red';
-        if(feature.getProperty('Indice_emv') >= 4) {//milieu vulnérable et prioritaire
-          fillOpacity = 0.7;
-        } else if(feature.getProperty('Indice_emv') === 3) {//milieu vulnérable et non prioritaire
-          fillOpacity = 0.5;
-        } else {// milieu non vulnérable
-          fillOpacity = 0.3;
-        }
+        strokeColor = '#F01FB7';
+        fillColor = '#F01FB7';
+        fillOpacity = 0.1;
       }
       return {
           fillColor: fillColor,
@@ -215,7 +196,7 @@ const MapComponent = ({filtersMap}) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        onValue(ref(database, 'properties'), (snapshot) => {
+        onValue(ref(database, 'properties'), (snapshot) => {debugger
           const data = snapshot.val();
           setDataProperties(data.reduce((acc, p)=>{
             acc[`${p.city}`] = acc[`${p.city}`].concat([p])
